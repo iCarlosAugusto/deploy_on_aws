87769036c9144eb8ea52d23a35d8e40efefaaffe
iCarlosAugusto <augusto.dev@proton.me>
2024-06-02 21:58:02 -0300

    init

---
+++ src/main/java/com/projects/deploy_aws/DeployAwsApplication.java
@@ -0,0 +1,13 @@
+package com.projects.deploy_aws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeployAwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeployAwsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projects/deploy_aws/DeployAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.deploy_aws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeployAwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
